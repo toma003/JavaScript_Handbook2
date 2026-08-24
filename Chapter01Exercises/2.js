95dc8e1a199c2813813d09f2d3a2507e6a986465
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-24 21:56:56 +0600

    Create 2.js

---
+++ Chapter01Exercises/2.js
@@ -0,0 +1,7 @@
+// 2. Write a program with a comment on top describing what it does, then print any sentence. 
+
+// This is a single-line comment.
+/* This is a multi-line comment.
+   It can span several lines. */
+
+console.log("Comments do not print.")
