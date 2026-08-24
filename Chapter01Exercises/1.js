aaa9df67c06f754745e2b7d4ee724d80d2fdff6c
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-24 21:51:38 +0600

    Create 1.js

---
+++ Chapter01Exercises/1.js
@@ -0,0 +1,4 @@
+// 1. Print your name, your department, and your university on three separate lines. 
+console.log ("Hi, i am mangoo!")
+console.log ("Department: CSE")
+console.log ("Metropolitan University.")
