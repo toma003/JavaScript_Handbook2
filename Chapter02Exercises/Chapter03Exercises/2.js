19cd380a65b58004be116fa2949d27460e16ec83
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-25 21:21:08 +0600

    Create 2.js

---
+++ Chapter02Exercises/Chapter03Exercises/2.js
@@ -0,0 +1,7 @@
+// 2. Given a temperature variable, print 'Hot' above 32, 'Pleasant' between 20 and 32, otherwise 'Cold'.
+
+const temperature = 7;
+
+if(temperature > 32) console.log("The temperature is HOT");
+else if(temperature >= 20 && temperature <= 32) console.log("The temperature is Plesant");
+else console.log("The temperature is cold!");
