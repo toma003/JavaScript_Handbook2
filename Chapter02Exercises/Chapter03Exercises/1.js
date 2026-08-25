c69d9bc25e1b8ef0f285c5d24b221dd5dc2cb66c
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-25 21:20:38 +0600

    Create 1.js

---
+++ Chapter02Exercises/Chapter03Exercises/1.js
@@ -0,0 +1,10 @@
+// 1. Write a program that prints whether a number is positive, negative, or zero.
+
+const num = 40;
+
+if(num > 0) console.log(`The number ${num} is positive.`);
+else if(num == 0) console.log(`The number ${num} is zero.`);
+else console.log(`The number ${num} is negative.`);
+
+
+
