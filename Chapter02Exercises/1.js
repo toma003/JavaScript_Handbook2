2f7ce8510ca3eefb5de00be3c89ed5c253f55011
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-25 20:17:52 +0600

    Create 1.js

---
+++ Chapter02Exercises/1.js
@@ -0,0 +1,7 @@
+// 1. Create const variables for your name and department and a let variable for your semester. Print a sentence using a template literal. 
+
+const name = "Kabir";
+const department = "CSE";
+let semester = "7th";
+
+console.log (`Hello, I am ${name} from the ${department}, just finished my ${semester} semester:)`);
