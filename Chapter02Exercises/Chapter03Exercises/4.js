3dbd341dc6fb4c30eb414e00d553a06a89e4ef72
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-25 22:01:11 +0600

    Create 4.js

---
+++ Chapter02Exercises/Chapter03Exercises/4.js
@@ -0,0 +1,29 @@
+// 4. Write a switch that maps a month number (1–12) to its season in Bangladesh.
+
+const monthNumber = 1;
+
+switch (monthNumber) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        console.log("Spring");
+        break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+        console.log("Summer");
+        break;
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+        console.log("Autumn");
+        break;
+    default:
+        console.log("Invalid!");
+}
+
+
+
