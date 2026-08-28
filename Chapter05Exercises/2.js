28fe886b27041f4318b572b70e3063c35415e49c
Tomalika Paul Toma <144379705+toma003@users.noreply.github.com>
2026-08-28 22:21:14 +0600

    Create 2.js

---
+++ Chapter05Exercises/2.js
@@ -0,0 +1,10 @@
+// 2. Write celsiusToFahrenheit(c) using the formula F = C × 9/5 + 32.
+
+function  celsiusToFahrenheit(c) {
+
+    return (c * (9/5)) + 32;
+}
+
+let c = 32;
+
+console.log(`Celsius ${c}, Fahrenheit: ${celsiusToFahrenheit(c)}`);
